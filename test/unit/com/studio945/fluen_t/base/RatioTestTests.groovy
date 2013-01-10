c3cdda9c0e230d3a222ec1e47f1899f69e2811e5
DJ Daugherty <ddaugher@gmail.com>
2013-01-10 00:23:31 -0500

    handling rounding to 6 decimal positions

--- test/unit/com/studio945/fluen_t/base/RatioTestTests.groovy
+++ test/unit/com/studio945/fluen_t/base/RatioTestTests.groovy
@@ -17,28 +17,31 @@ class RatioTestTests {
 
     @Test
     void shouldReturnProperNumberWhenRoundingIsNotRequired() {
-        Ratio r3over2 = Ratio.of(new BigDecimal(3), new BigDecimal(2));
+        def r3over2 = Ratio.of(new BigDecimal(3), new BigDecimal(2));
         def result = r3over2.decimalValue(1, Rounding.UNNECESSARY);
         assertEquals(new BigDecimal("1.5"), result);
     }
 
     @Test
     void shouldReturnProperNumberWhenRoundingDown() {
-        Ratio r10over3 = Ratio.of(new BigDecimal(10), new BigDecimal(3));
+        def r10over3 = Ratio.of(new BigDecimal(10), new BigDecimal(3));
         def result = r10over3.decimalValue(3, Rounding.DOWN);
         assertEquals(new BigDecimal("3.333"), result);
     }
 
     @Test
     void shouldReturnProperNumberWhenRoundingUp() {
-        Ratio r10over3 = Ratio.of(new BigDecimal(10), new BigDecimal(3));
+        def r10over3 = Ratio.of(new BigDecimal(10), new BigDecimal(3));
         def result = r10over3.decimalValue(3, Rounding.UP);
         assertEquals(new BigDecimal("3.334"), result);
     }
-//        Ratio rManyDigits = Ratio.of(new BigDecimal("9.001"), new BigDecimal(3));
-//        result = rManyDigits.decimalValue(6, Rounding.UP);
-//        assertEquals(new BigDecimal("3.000334"), result);
-//
+
+    @Test
+    void shouldReturnProperNumberWhenRoundingUpToSixDecimalPositions() {
+        def rManyDigits = Ratio.of(new BigDecimal("9.001"), new BigDecimal(3));
+        def result = rManyDigits.decimalValue(6, Rounding.UP);
+        assertEquals(new BigDecimal("3.000334"), result);
+    }
 //        result = rManyDigits.decimalValue(7, Rounding.UP);
 //        assertEquals(new BigDecimal("3.0003334"), result);
 //
